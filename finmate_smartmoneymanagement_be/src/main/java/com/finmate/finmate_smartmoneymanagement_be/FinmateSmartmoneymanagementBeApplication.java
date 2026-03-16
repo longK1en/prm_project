45946e18b41c53031b44b7edfe2529bc4167b58f
longK1en <longkien2504@gmail.com>
2026-03-16 12:57:43 +0700

    Initial commit: PRM Project (Flutter FE + Spring Boot BE)

---
+++ finmate_smartmoneymanagement_be/src/main/java/com/finmate/finmate_smartmoneymanagement_be/FinmateSmartmoneymanagementBeApplication.java
@@ -0,0 +1,13 @@
+package com.finmate.finmate_smartmoneymanagement_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmateSmartmoneymanagementBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinmateSmartmoneymanagementBeApplication.class, args);
+    }
+
+}
---
+++ finmate_smartmoneymanagement_be/src/test/java/com/finmate/finmate_smartmoneymanagement_be/FinmateSmartmoneymanagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finmate.finmate_smartmoneymanagement_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinmateSmartmoneymanagementBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
